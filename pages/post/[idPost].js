20c894353a815d524cf17c8ce52eff74fa25e12d
insurecar <rostyslavshyian@gmail.com>
2021-04-21 13:27:57 +0300

    add local loading

--- pages/post/[idPost].js
+++ pages/post/[idPost].js
@@ -1,9 +1,32 @@
 import { MainLayout } from "../../components/MainLayout";
 import axios from "axios";
 import Link from "next/link";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 
-export default function Post({ post }) {
-  console.log(post);
+export default function Post({ post: serverPost }) {
+  const [post, setPost] = useState(serverPost);
+  const router = useRouter();
+
+  useEffect(() => {
+    async function load() {
+      const response = await axios(
+        `http://localhost:4200/posts/${router.query.idPost}`
+      );
+      const data = await response.data;
+      setPost(data);
+    }
+    if (!serverPost) load();
+  }, []);
+
+  if (!post) {
+    return (
+      <MainLayout>
+        <p>Loading</p>
+      </MainLayout>
+    );
+  }
+
   return (
     <MainLayout>
       <h1>Post:{post.title}</h1>
@@ -14,9 +37,13 @@ export default function Post({ post }) {
   );
 }
 
-Post.getInitialProps = async ({ query: { idPost } }) => {
-  console.log(idPost);
-  const response = await axios(`http://localhost:4200/posts/${idPost}`);
+Post.getInitialProps = async ({ query, req }) => {
+  if (!req) {
+    return {
+      post: null,
+    };
+  }
+  const response = await axios(`http://localhost:4200/posts/${query.idPost}`);
   const post = await response.data;
 
   return { post };
--- pages/posts.js
+++ pages/posts.js
@@ -3,19 +3,35 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import Link from "next/link";
 
-export default function Posts({ result }) {
-  // const [posts, setPosts] = useState([]);
-  // useEffect(() => {
-  //   axios
-  //     .get("http://localhost:4200/posts")
-  //     .then((resp) => setPosts(resp.data));
-  // }, []);
+export default function Posts({ posts: serverPost }) {
+  console.log(serverPost);
+  const [posts, setPosts] = useState(serverPost);
+
+  useEffect(() => {
+    async function load() {
+      const resp = await axios.get("http://localhost:4200/posts");
+      const data = await resp.data;
+
+      setPosts(data);
+    }
+    if (!serverPost) {
+      load();
+    }
+  }, []);
+
+  if (!posts) {
+    return (
+      <MainLayout>
+        <p>Loading...</p>
+      </MainLayout>
+    );
+  }
 
   return (
     <MainLayout>
       <h1>Posts Page</h1>
       <ul>
-        {result.map((post) => (
+        {posts.map((post) => (
           <li key={post.id}>
             <Link href={`/post/[idPost]`} as={`/post/${post.id}`}>
               <a>{post.title}</a>
@@ -27,8 +43,15 @@ export default function Posts({ result }) {
   );
 }
 
-export async function getStaticProps() {
+export async function getStaticProps({ req }) {
+  if (!req) {
+    return {
+      props: {
+        posts: null,
+      },
+    };
+  }
   const resp = await axios.get("http://localhost:4200/posts");
-  const result = await resp.data;
-  return { props: { result } };
+  const posts = await resp.data;
+  return { props: { posts } };
 }
